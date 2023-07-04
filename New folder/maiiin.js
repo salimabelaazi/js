77f074dc688d69dc5491a559613dc08ac404cf8a
salimabelaazi <salimabelaazi20@gmail.com>
2023-07-04 03:28:11 -0700

    myapplic

---
+++ New folder/maiiin.js
@@ -0,0 +1,20 @@
+// let d =1_00;
+// let e =2_00.5;
+// let f = 1e2;
+// let g = 2.4;
+
+
+// //console.log(math.trunc(math.min(d, e, f, g)))
+
+// let a ="2";
+// console.log(number(a));
+// console.log(+a);
+
+let a = "Elzero web school";
+ 
+console.log(a.charAt(2).toUpperCase()+a.slice(3,6));
+
+
+console.log(a.charAt(13).toUpperCase().repeat(8));
+
+console.log(a.split("",1))
---
+++ myapp/src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import {Nav, NavDropdown} from 'react-bootstrap';
+
+function App() {
+  return (
+    <React.fragment>
+      <div className="App">
+      <Nav variant="pills" >
+      <Nav.Item>
+        <Nav.Link eventKey="1" href="#/home">
+          NavLink 1 content
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="2" title="Item">
+          NavLink 2 content
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="3" disabled>
+          NavLink 3 content
+        </Nav.Link>
+      </Nav.Item>
+      <NavDropdown title="Dropdown" id="nav-dropdown">
+        <NavDropdown.Item eventKey="4.1">Action</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item eventKey="4.3">Something else here</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item eventKey="4.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+      </div>
+    </React.fragment>
+  );
+}
+
+export default App;
